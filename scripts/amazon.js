b36fef8dcbe0a5459b46f25b920fae32e18a3413
Bhavi13541 <bhavitha281reddy@gmail.com>
2026-06-14 11:27:40 +0530

    make add to button cart

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -48,12 +48,12 @@ products.forEach((product) => {
         Added
       </div>
 
-        <button class="add-to-cart-button button-primary">
+        <button class="add-to-cart-button button-primary js-add-to-cart
+        data-product-id=${product.id}">
           Add to Cart
         </button>
     </div>`
     ;
-
 
 
 });
@@ -61,3 +61,28 @@ products.forEach((product) => {
 console.log(productsHTML)
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart')
+  .forEach((button) => {
+    button.addEventListener('click', () => {
+      const productId = button.database.productId;
+
+      let matchingItem;
+
+      cart.forEach((item) => {
+        if (productId === item.productId) {
+          matchingItem = item;
+        }
+      });
+
+      if (matchingItem) {
+        matchingItem.quantity += 1
+      } else {
+        cart.push({
+          productId: productId,
+          quantity: 1
+        });
+      }
+
+    });
+  });
